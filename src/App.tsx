219d1c9746161bb9cd6a2bd7bbbafd594ea397ac
Gil Hanan <gil.hanan@gmail.com>
2023-12-24 11:42:30 +0200

    ScoresPercentages configurable

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import {
 } from "chart.js";
 import DurationsChat from "./DurationsChat";
 import { getData } from "./data";
-import { Metric, Url, UrlRow, UrlRows } from "./models";
+import { Metric, ScoresPercentages, Url, UrlRow, UrlRows } from "./models";
 import { scoresParameters } from "./constants";
 import { computeLogNormalScore } from "./lighthouse/audit";
 
@@ -27,6 +27,8 @@ ChartJS.register(
 );
 
 function App() {
+  const [parameters, setParameters] =
+    useState<ScoresPercentages>(scoresParameters);
   const [data, setData] = useState<UrlRows>({});
 
   async function loadData() {
@@ -53,13 +55,13 @@ function App() {
             (row) =>
               Object.fromEntries(
                 Object.entries(row).map(([metric, value]) => {
-                  const { p10 } =
-                    scoresParameters[url as Url][metric as Metric];
+                  const { p10, median } =
+                    parameters[url as Url][metric as Metric];
                   const score =
                     computeLogNormalScore(
                       {
-                        p10: p10,
-                        median: p10 * 2,
+                        p10,
+                        median,
                       },
                       value
                     ) * 100;
@@ -78,14 +80,79 @@ function App() {
     );
   }
 
+  function Parameters() {
+    return (
+      <div className="flex">
+        {Object.entries(parameters).map(([url, rows]) => (
+          <div key={url} className="flex flex-col border">
+            <div className="p-2 font-bold">{url}</div>
+            <div className="flex">
+              <div className="p-2">
+                {Object.keys(rows).map((metric) => (
+                  <div key={metric}>{metric}</div>
+                ))}
+              </div>
+              <div className="p-2">
+                {Object.entries(rows).map(([metric, { p10, median }]) => (
+                  <div key={metric}>
+                    <input
+                      type="number"
+                      className="w-16"
+                      value={p10}
+                      min={1}
+                      max={median}
+                      onInput={(e) => {
+                        setParameters({
+                          ...parameters,
+                          [url]: {
+                            ...parameters[url as Url],
+                            [metric as Metric]: {
+                              ...[parameters[url as Url][metric as Metric]],
+                              p10: Number(e.currentTarget.value),
+                            },
+                          },
+                        });
+                      }}
+                    />
+                    <input
+                      type="number"
+                      className="w-16"
+                      value={median}
+                      min={p10}
+                      onInput={(e) => {
+                        setParameters({
+                          ...parameters,
+                          [url]: {
+                            ...parameters[url as Url],
+                            [metric as Metric]: {
+                              ...[parameters[url as Url][metric as Metric]],
+                              metric: Number(e.currentTarget.value),
+                            },
+                          },
+                        });
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   useEffect(() => {
     loadData();
   }, []);
 
   return (
-    <div className="flex flex-col gap-8">
-      <DurationsCharts />
-      <ScoresCharts />
+    <div className="p-4">
+      <Parameters />
+      <div className="flex flex-col gap-8">
+        <DurationsCharts />
+        <ScoresCharts />
+      </div>
     </div>
   );
 }
